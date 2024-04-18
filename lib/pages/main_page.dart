11bc4c686b75ad87bdd169c5d844a4f3b3d16477
jramilm <malatejramil@gmail.com>
2024-04-18 23:18:41 +0800

    add UI on pomodoro page

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -4,8 +4,6 @@ import 'package:pomodoro_app/pages/pomodoro_page.dart';
 import 'package:pomodoro_app/pages/profile_page.dart';
 import 'package:pomodoro_app/pages/topic_page.dart';
 
-
-
 class Mainpage extends StatefulWidget {
   const Mainpage({super.key});
 
@@ -14,10 +12,9 @@ class Mainpage extends StatefulWidget {
 }
 
 class _MainpageState extends State<Mainpage> {
-
   int currentIndex = 0;
   final screens = [
-    pomodoro_page(),
+    PomodoroPage(),
     flashcard_page(),
     topic_page(),
     profile_page(),
@@ -26,39 +23,51 @@ class _MainpageState extends State<Mainpage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-        body: IndexedStack(
-          index: currentIndex,
-          children: screens,
-          ),
-
-        bottomNavigationBar: BottomNavigationBar(
-          currentIndex: currentIndex,
-          onTap: (value) => setState(() => currentIndex = value),
-          items: [
+      body: IndexedStack(
+        index: currentIndex,
+        children: screens,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (value) => setState(() => currentIndex = value),
+        items: [
           BottomNavigationBarItem(
             icon: Container(
               height: 30,
               child: Image.asset(
                 'icons/pie-chart.png',
-                color: Theme.of(context).colorScheme.secondary,),
-                ),
+                color: Theme.of(context).colorScheme.secondary,
+              ),
+            ),
             label: 'Pomodoro',
           ),
           BottomNavigationBarItem(
-            icon: Container(height: 30,child: Image.asset('icons/document.png', color: Theme.of(context).colorScheme.secondary,),),
-            label: 'Flash Card'
-          ),
+              icon: Container(
+                height: 30,
+                child: Image.asset(
+                  'icons/document.png',
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+              ),
+              label: 'Flash Card'),
           BottomNavigationBarItem(
-            icon: Container(height: 30,child: Image.asset('icons/bill.png', color: Theme.of(context).colorScheme.secondary,),),
-            label: 'Topics'
-          ),
+              icon: Container(
+                height: 30,
+                child: Image.asset(
+                  'icons/bill.png',
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+              ),
+              label: 'Topics'),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person_outlined, color: Theme.of(context).colorScheme.secondary, size: 35,),
-            label: 'Profile'
-          )
+              icon: Icon(
+                Icons.person_outlined,
+                color: Theme.of(context).colorScheme.secondary,
+                size: 35,
+              ),
+              label: 'Profile')
         ],
-        ),
-      );
+      ),
+    );
   }
 }
