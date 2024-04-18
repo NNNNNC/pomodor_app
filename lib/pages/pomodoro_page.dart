e488c082255dd2b6dc335aaacd1489bbe1811096
jramilm <malatejramil@gmail.com>
2024-04-19 05:33:46 +0800

    add static interactions on focus button

--- lib/pages/pomodoro_page.dart
+++ lib/pages/pomodoro_page.dart
@@ -1,15 +1,73 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:pomodoro_app/utils/topic_dialog.dart';
-// import 'package:flutter/widgets.dart';
 
 class PomodoroPage extends StatefulWidget {
   const PomodoroPage({super.key});
-
   @override
   State<PomodoroPage> createState() => _PomodoroPageState();
 }
 
 class _PomodoroPageState extends State<PomodoroPage> {
+  bool isFocusing = false;
+  bool isBreak = false;
+  bool isLongBreak = false;
+  bool timerStarted = false;
+  Timer? timer;
+  int seconds = 0, minutes = 0;
+  String digitSec = '00', digitMin = '00';
+  List laps = [];
+
+  // timer methods
+
+  void timerStop() {
+    timer!.cancel();
+    setState(() {
+      timerStarted = false;
+    });
+  }
+
+  void reset() {
+    timer!.cancel();
+    setState(() {
+      seconds = 59;
+      minutes = 25;
+      digitMin = '25';
+      digitSec = '00';
+      timerStarted = false;
+    });
+  }
+
+  void addLaps() {
+    String lap = "$digitMin:$digitSec";
+    setState(() {
+      laps.add(lap);
+    });
+  }
+
+  void startTimer() {
+    timerStarted = true;
+
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      int localSeconds = seconds - 1;
+      int localMinutes = minutes;
+
+      if (localSeconds < 1) {
+        localMinutes--;
+        localSeconds = 59;
+      }
+      setState(() {
+        seconds = localSeconds;
+        minutes = localMinutes;
+        digitSec = (seconds >= 10) ? "$seconds" : "0$seconds";
+        digitMin = (seconds >= 10) ? "$minutes" : "0$minutes";
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,25 +84,6 @@ class _PomodoroPageState extends State<PomodoroPage> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 TextButton(
-                  /*onPressed: () => showMenu(
-                    context: context,
-                    position:
-                        const RelativeRect.fromLTRB(30.0, 150.0, 100.0, 150.0),
-                    items: const [
-                      PopupMenuItem(
-                        child: Text('Topic 1'),
-                      ),
-                      PopupMenuItem(
-                        child: Text('Topic 2'),
-                      ),
-                      PopupMenuItem(
-                        child: Text('Topic 3'),
-                      ),
-                      PopupMenuItem(
-                        child: Text('Topic 4'),
-                      ),
-                    ],
-                  ),*/
                   onPressed: () => showDialog(
                     context: context,
                     builder: (context) {
@@ -100,96 +139,123 @@ class _PomodoroPageState extends State<PomodoroPage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 // Focus state
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(77),
-                      bottomLeft: Radius.circular(77),
-                      topRight: Radius.circular(16),
-                      bottomRight: Radius.circular(16),
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 1.5,
-                        spreadRadius: 0,
-                        offset: const Offset(-4, 4),
-                        color: Colors.black.withOpacity(0.25),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isFocusing = !isFocusing;
+                      isBreak = false;
+                      isLongBreak = false;
+                    });
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: isFocusing ? Colors.grey[900] : Colors.grey,
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(77),
+                        bottomLeft: Radius.circular(77),
+                        topRight: Radius.circular(16),
+                        bottomRight: Radius.circular(16),
                       ),
-                    ],
-                  ),
-                  height: 32,
-                  width: 71,
-                  child: const Center(
-                    child: Text(
-                      'Focus',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold,
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 1.5,
+                          spreadRadius: 0,
+                          offset: const Offset(-4, 4),
+                          color: Colors.black.withOpacity(0.25),
+                        ),
+                      ],
+                    ),
+                    height: 32,
+                    width: 71,
+                    child: const Center(
+                      child: Text(
+                        'Focus',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ),
                 ),
 
                 // Long Break State
-                Container(
-                  margin: const EdgeInsets.only(left: 5.0),
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: const BorderRadius.all(Radius.circular(6)),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 1.5,
-                        spreadRadius: 0,
-                        offset: const Offset(0, 4),
-                        color: Colors.black.withOpacity(0.25),
-                      ),
-                    ],
-                  ),
-                  height: 32,
-                  width: 90,
-                  child: const Center(
-                    child: Text(
-                      'Long Break',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold,
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isLongBreak = !isLongBreak;
+                      isBreak = false;
+                      isFocusing = false;
+                    });
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 5.0),
+                    decoration: BoxDecoration(
+                      color: isLongBreak ? Colors.grey[900] : Colors.grey,
+                      borderRadius: const BorderRadius.all(Radius.circular(6)),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 1.5,
+                          spreadRadius: 0,
+                          offset: const Offset(0, 4),
+                          color: Colors.black.withOpacity(0.25),
+                        ),
+                      ],
+                    ),
+                    height: 32,
+                    width: 90,
+                    child: const Center(
+                      child: Text(
+                        'Long Break',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ),
                 ),
 
                 // Short break state
-                Container(
-                  margin: const EdgeInsets.only(left: 5.0),
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(16),
-                      bottomLeft: Radius.circular(16),
-                      topRight: Radius.circular(77),
-                      bottomRight: Radius.circular(77),
-                    ),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 1.5,
-                        spreadRadius: 0,
-                        offset: const Offset(4, 4),
-                        color: Colors.black.withOpacity(0.25),
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isLongBreak = false;
+                      isBreak = !isBreak;
+                      isFocusing = false;
+                    });
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 5.0),
+                    decoration: BoxDecoration(
+                      color: isBreak ? Colors.grey[900] : Colors.grey,
+                      borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(16),
+                        bottomLeft: Radius.circular(16),
+                        topRight: Radius.circular(77),
+                        bottomRight: Radius.circular(77),
                       ),
-                    ],
-                  ),
-                  height: 32,
-                  width: 71,
-                  child: const Center(
-                    child: Text(
-                      'Break',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
-                        fontWeight: FontWeight.bold,
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 1.5,
+                          spreadRadius: 0,
+                          offset: const Offset(4, 4),
+                          color: Colors.black.withOpacity(0.25),
+                        ),
+                      ],
+                    ),
+                    height: 32,
+                    width: 71,
+                    child: const Center(
+                      child: Text(
+                        'Break',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
                   ),
@@ -203,44 +269,71 @@ class _PomodoroPageState extends State<PomodoroPage> {
           ),
 
           // Focus Button
-          Container(
-            width: 251,
-            height: 251,
-            decoration: BoxDecoration(
-              color: const Color(0xff3a3939),
-              shape: BoxShape.circle,
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 1.5,
-                  spreadRadius: 7,
-                  offset: Offset.zero,
-                  color: Colors.black.withOpacity(0.25),
-                )
-              ],
-            ),
-            child: const Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    'FOCUS',
-                    style: TextStyle(
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                  SizedBox(height: 10),
-                  Text(
-                    'Click here to start pomodoro',
-                    style: TextStyle(
-                      color: Color(0xffc0c0c0),
-                      fontSize: 14,
-                      fontWeight: FontWeight.w500,
-                    ),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                isFocusing = !isFocusing;
+                isBreak = false;
+                isLongBreak = false;
+              });
+            },
+            child: Container(
+              width: 251,
+              height: 251,
+              decoration: BoxDecoration(
+                color: isFocusing == true
+                    ? const Color(0xffc50e0e)
+                    : isBreak == true
+                        ? const Color(0xff1dc50e)
+                        : isLongBreak == true
+                            ? const Color(0xff0e15c5)
+                            : const Color(0xff3a3939),
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 1.5,
+                    spreadRadius: 7,
+                    offset: Offset.zero,
+                    color: Colors.black.withOpacity(0.25),
                   ),
                 ],
+              ),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      isFocusing
+                          ? 'FOCUSING'
+                          : isBreak
+                              ? 'BREAK'
+                              : isLongBreak
+                                  ? 'LONG BREAK'
+                                  : 'FOCUS',
+                      style: const TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    Text(
+                      isFocusing
+                          ? '25:00'
+                          : isBreak
+                              ? '05:00'
+                              : isLongBreak
+                                  ? '15:00'
+                                  : 'Click here to start pomodoro',
+                      style: const TextStyle(
+                        color: Color(0xffc0c0c0),
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
